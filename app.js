601f672e31f79ee1f22f739b023c093574a8cb33
AceOf5pades <36089338+AceOf5pades@users.noreply.github.com>
2018-02-02 23:07:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ client.on("guildCreate", guild => {
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.setGame(`https://projectacemodz.com | On ${client.guilds.size} server`);
 });
 
 
