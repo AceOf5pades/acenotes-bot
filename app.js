42b3c1c993a0c4dcda8811cc30526ab803ac74cb
AceOf5pades <36089338+AceOf5pades@users.noreply.github.com>
2018-02-02 23:12:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.setGame(`https://projectacemodz.com | On ${client.guilds.size} server`);
 });
 
 client.on("guildCreate", guild => {
