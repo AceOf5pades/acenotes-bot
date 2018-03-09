545440bab069b73f0654bebafad656d38f197d90
AceOf5pades <36089338+AceOf5pades@users.noreply.github.com>
2018-03-09 15:45:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ client.on("message", async message => {
     // This command removes all messages from all users in the channel, up to 100.
     
     // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 1);
+    const deleteCount = parseInt(args[0], 10);
     
     // Ooooh nice, combined conditions. <3
     if(!deleteCount || deleteCount < 1 || deleteCount > 100)
