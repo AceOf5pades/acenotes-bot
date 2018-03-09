acafc10f699f6584c3b8fadf7d1868dd7525fae8
AceOf5pades <36089338+AceOf5pades@users.noreply.github.com>
2018-03-08 21:58:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const Discord = require("discord.js");
 // this is what we're refering to. Your client.
 const client = new Discord.Client();
 
-const config = require("./config.json");
-
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
@@ -152,4 +150,3 @@ message.channel.send({embed});
 });
 
 client.login(process.env.BOT_TOKEN);
-           
